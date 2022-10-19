f837ed806354cc57e4c1ba9fe8e869a96f89719f
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-10-19 14:46:17 -0500

    PublicRouter para manejar la ruta publica (LoginPage)

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../auth/pages/LoginPage";
 import HeroesRoutes from "../heroes/routes/HeroesRoutes";
 import { PrivateRouter } from "./PrivateRouter";
+import { PublicRouter } from "./PublicRouter";
 
 export default function AppRouter() {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={
+          <PublicRouter>
+            <LoginPage/>
+          </PublicRouter>
+        } />
         <Route path="/*" element= {
           <PrivateRouter>
             <HeroesRoutes />
