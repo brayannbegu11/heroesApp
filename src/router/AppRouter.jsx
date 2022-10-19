c53bbeaa9cb144e6ace83a07abe22e7a2347ab7f
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-10-19 14:35:39 -0500

    Router para rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../auth/pages/LoginPage";
 import HeroesRoutes from "../heroes/routes/HeroesRoutes";
+import { PrivateRouter } from "./PrivateRouter";
 
 export default function AppRouter() {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route path="/*" element= {
+          <PrivateRouter>
+            <HeroesRoutes />
+          </PrivateRouter>
+        } />
       </Routes>
     </>
   );
