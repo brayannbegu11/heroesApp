7ff50b4429061efb46856b61c39c601024c2fbd0
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-10-19 14:13:41 -0500

    Se creó la función del login para crear un usuario al ingresar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 export default function LoginPage() {
   const navigate = useNavigate()
+const {login} = useContext(AuthContext)
+
   const onLogin =() => {
+
+    login('Brayann')
+    
     navigate('/marvel', {
       replace: true
     })
